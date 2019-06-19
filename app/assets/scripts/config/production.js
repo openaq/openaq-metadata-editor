26ddaadcf4df467adfac870ee5f012b5e1e0b5f6
Daniel da Silva <daniel@developmentseed.org>
2019-06-19 17:04:01 +0100

    Add auth0 docs

--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -8,8 +8,12 @@ export default {
   },
   apiUrl: null,
   auth: {
+    // Domain and client can be found in the Auth0 Application page.
     domain: null,
     clientID: null,
-    callbackUrl: null
+    // App url + /callback. Ex: http://localhost:3000/callback
+    callbackUrl: null,
+    // Audience is the identifier chosen for the Api. Ex: http://openaq.org
+    audience: null
   }
 };
